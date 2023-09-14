f768447e7919860ef6078731f259eb247589e87a
Devon <devon.stettler@tutanota.com>
2023-09-14 01:18:37 -0400

    Fix rbm-converter

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 This repo is the home of psb-converter, a program that converts raw image arrays (which can be generated from common formats with `rbm-converter.py`) into my `psb` image format.
 It's mostly just a way for me to play around with concepts behind image compression, and isn't anything too serious. 
 I'll update this README as I work out a, well, working version.
+
+### `rbm-converter.py`
+
+Pip Install Requirements:
+- `Pillow`
+- `numpy`
--- rbm-converter.py
+++ rbm-converter.py
@@ -2,9 +2,24 @@ from PIL import Image
 import numpy as np
 import argparse
 
-filename = 'image'
-path = f'{filename}.png'
-image = Image.open(path)
+parser = argparse.ArgumentParser(prog='RBM Converter',description='Converts images into raw RGB(A) arrays')
+parser.add_argument('filename')
+parser.add_argument('-o', '--output', required=False, help='File to output rbm contents to')
+
+args = parser.parse_args()
+
+filepath: str = args.filename
+split_filepath: list[str] = filepath.split('.')
+filename: str = ''
+
+# Remove the file extension
+for i, text in enumerate(split_filepath):
+    if i < len(split_filepath) - 1:
+        filename += text
+    else:
+        break
+
+image = Image.open(filepath)
 image = np.asarray(image)
 
 baselist = []
@@ -13,7 +28,12 @@ for i in range(image.shape[0]):
     for j in range(image.shape[1]):
         baselist.append(image[i][j])
                         
-with open(f'{filename}.rbm', 'w') as f:
+
+rbm_name = f'{filename}.rbm'
+if args.output != None:
+    rbm_name = args.output
+
+with open(rbm_name, 'w') as f:
     f.write(f"{image.shape[0]},{image.shape[1]}\n")
     for line in baselist:
         blist = list(line.tobytes())
